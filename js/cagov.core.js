3f0abaf93841d30db79c021ea679617325aefba0
tony chang <tony.chang@digital.ca.gov>
2020-11-02 16:14:59 -0800

    Add a time picker widget.
    Styled as Christian sent along with logic
    for what should happen on blur.
    
    I think this is the correct a11y attributes,
    but let me know if I missed anything.

--- js/cagov.core.js
+++ js/cagov.core.js
@@ -19395,3 +19395,43 @@ $(document).ready(function () {
     $(".settings-links button[data-target='#siteSettings']").removeAttr("role aria-selected");
     $(".site-settings button.close").removeAttr("role aria-selected");
 });
+
+/* -----------------------------------------
+   Time pickers
+----------------------------------------- */
+$(document).ready(function () {
+  $(".timepicker input[type=text]").on("blur", function (e) {
+    e.target.value = e.target.value.replace(/^(\d?\d):?(\d\d)?$/, function(match, hh, mm) {
+      //console.log(match, hh, mm);
+      var hhInt = parseInt(hh, 10);
+      if (hhInt < 0 || hhInt > 23) {
+        return match;
+      }
+
+      // Fill in minutes if not specified.
+      if (typeof mm === "undefined") {
+        mm = "00";
+      }
+
+      var mmInt = parseInt(mm, 10);
+      if (mmInt < 0 || mmInt > 59) {
+        return match;
+      }
+
+      var timepicker = $(e.target).parent().parent();
+      // Adjust from military time.
+      if (hhInt > 12) {
+        timepicker.find(".form-check input[type=radio]")[1].checked = true;
+        hh = (hhInt - 12).toString();
+      } else if (hhInt === 0) {
+        timepicker.find(".form-check input[type=radio]")[0].checked = true;
+        hh = "12";
+      }
+
+      if (hh.length === 1) {
+        hh = "0" + hh;
+      }
+      return hh + ":" + mm;
+    });
+  });
+});
